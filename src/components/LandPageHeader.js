8d97768c858b17076ecb1c8775b4654ad730618d
Sergio Giraldez Moreno <sergi.giraldez.moreno@gmail.com>
2022-03-17 20:57:57 +0100

    Add animation for the header

--- src/components/LandPageHeader.js
+++ src/components/LandPageHeader.js
@@ -1,25 +1,47 @@
 const LandPageHeader = ({isExpanded}) => {
   return (
-    // TODO: ADD ANIMATION FOR DE TRANSITION
     <>
-      <div className={isExpanded ? 'expanded-header p-4 text-dark' : 'd-none'}>
-          <div className="col-12 h-25 d-flex justify-content-center align-items-center">
-            <h1 className="fw-bold">Welcome to Goal Keeper</h1>
+      <div className={isExpanded ? 'expanded-header p-4 text-dark' : 'hide-slow bg-dark'}>
+          <div className={isExpanded ? "col-12 h-40 d-flex justify-content-center align-items-center": 'd-none'}>
+            <h1 className="fw-bold big-title">Welcome to Goal Keeper</h1>
           </div>
-          <div className="row h-25">
-            <img className='h-100 w-auto m-auto' src={require('../images/trophy.png')} alt="" />
+          <div className={isExpanded ? "row h-25": 'd-none'}>
+            <img className='h-100 w-auto m-auto' src={require('../images/trophy.png')} alt="trophy image" />
           </div>
-          <div className="row h-25 col-10 text-center m-auto">
-              <a className="m-auto border-3 col-5 btn btn-outline-dark" href="#">Create account</a>
-              <a className="m-auto border-3 col-5 btn btn-outline-dark" href="#">Log in</a>
+          <div className={isExpanded ? "row justify-content-center h-25 col-10 text-center m-auto": 'd-none'}>
+              {/* TODO: IF LOGGED IN SHOW THE NAME INSTEAD OF THE BUTTONS */}
+              <a className="my-auto border-3 col-5 col-lg-3 btn btn-outline-dark" href="#">Create account</a>
+              <span className='col-1'></span>
+              <a className="my-auto border-3 col-5 col-lg-3 btn btn-outline-dark" href="#">Sign in</a>
           </div>
-          <div className="row h-25 d-flex flex-column justify-content-center align-items-end">
+          <div className={isExpanded ? "row h-10 d-flex flex-column justify-content-center align-items-end": 'd-none'}>
             <p className='mt-auto mb-0 text-center'>scroll down</p>
-            <img style={{height: "30px"}} className='mx-auto w-auto' src={require('../images/double-arrow.png')} alt="scroll down" />
+            <img className={isExpanded ? 'mx-auto w-auto land-page-scroll-img' : 'd-none'} src={require('../images/double-arrow.png')} alt="scroll down" />
           </div>
       </div>
       <div className={isExpanded ? 'd-none' : 'minimized-header text-light bg-dark text-center'}>
-        <p>main header</p>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark px-3">
+          <a className="navbar-brand" href="#">GoalKeeper</a> {/* TODO: ADD LINK TO TOP */}
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Our methods</a> {/* TODO: MOVE PAGE TO TITLE DOM */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Our whatever</a> {/* TODO: MOVE PAGE TO TITLE DOM */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Our team</a> {/* TODO: MOVE PAGE TO TITLE DOM */}
+              </li>
+            </ul>
+            {/* TODO: MY ACCOUNT BUTTON IF LOGGED IN INSTED OF SIGN IN */}
+            <button className='btn btn-light d-none d-md-block ms-auto'>sign in</button>
+            <button className='btn btn-light d-md-none col-8 mt-5 mb-2'>sign in</button>
+          </div>
+        </nav>
       </div>
     </>
     
--- src/components/LandPageOurMethods.js
+++ src/components/LandPageOurMethods.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LandPageOurMethods = () => {
   return (
-    <div className='h-100 text-center p-5'>
+    <div className='h-100 text-center container' style={{paddingTop: '8vh'}}>
         <h1>OUR METHODS</h1>
         <div className='border border-dark border-2 rounded-2 p-3'>
             <p>Here will be a brief introduction to our methodology to 
